523c1dcc334d57ade0b7b80fe97b8b19530d5394
Unknown <omar.maher.nassif@gmail.com>
2018-02-02 17:15:51 -0500

    Intermediate Plasticity 4
    I think what's left is to fix Fint

--- Materials/ClassicPlasticityRI.m
+++ Materials/ClassicPlasticityRI.m
@@ -14,7 +14,7 @@ classdef ClassicPlasticityRI
         kin_modulus
         yield_strss
         Shear
-        Bulk       
+        Bulk
         
         dt
         de_p
@@ -27,6 +27,11 @@ classdef ClassicPlasticityRI
         e_p
         Beta
         Alpha
+        
+        plastic = false;
+        sTrial
+        dGamma
+        dir;
     end
     %%
     methods
@@ -69,24 +74,41 @@ classdef ClassicPlasticityRI
             
         end
         %% Sigma
-        function sigma_voigt = Compute_cauchy(~, gp)
-            sigma_voigt = gp.D *gp.eps;
+        function sigma_voigt = Compute_cauchy(obj, gp)
+            if obj.plastic
+                G    = obj.Shear;
+                kappa= obj.Bulk;
+                sigma= kappa*sum(gp.eps(1:obj.ndm))*obj.I2 + obj.sTrial - 2*G*obj.dGamma*obj.dir;
+                sigma_voigt = [...
+                    sigma(1,1)
+                    sigma(2,2)
+                    sigma(3,3)
+                    sigma(1,2)
+                    sigma(2,3)
+                    sigma(1,3)];
+            else
+                sigma_voigt = gp.D *gp.eps;
+            end
         end
         %% Tangential stiffness
         function [D, ctan, obj] = Compute_tangentstiffness(obj, gp)
-            E = obj.Young;
-            v = obj.Poisson;
+            % Identities
+            I    = obj.I2;
+            I4   = obj.IFour;
+            % Material properties
+            E    = obj.Young;
+            v    = obj.Poisson;
             G    = obj.Shear;
             kappa= obj.Bulk;
             Kp   = obj.iso_modulus;
-            Hp   = obj.kin_modulus;
+            Hp   = obj.kin_modulus; 
             Y    = obj.yield_strss;
-            I    = obj.I2;
-            I4   = obj.IFour;
+            % Strain full matrix
             eps  = [...
                 1.0*gp.eps(1) 0.5*gp.eps(4) 0.5*gp.eps(6)
                 0.5*gp.eps(4) 1.0*gp.eps(2) 0.5*gp.eps(5)
                 0.5*gp.eps(6) 0.5*gp.eps(5) 1.0*gp.eps(3)];
+            
             obj.eps_np1(:,:,gp.i, gp.iel)= eps;
             
             ep     = obj.e_p  (:,:,gp.i, gp.iel);
@@ -94,15 +116,16 @@ classdef ClassicPlasticityRI
             alpha_n= obj.Alpha(    gp.i, gp.iel);
             
             e = eps - 1/3.*trace(eps)*I;
-%             [K_n,~,~,~] = obj.hardening(alpha_n);
-            K_n = ( Y + Kp*alpha_n ); 
+            %             [K_n,~,~,~] = obj.hardening(alpha_n);
+            K_n = ( Y + Kp*alpha_n );
             
-            sTrial      = 2*G*(e - ep);
-            xiTrial     = sTrial - beta;
-            normXiTrial = norm(xiTrial);
+            obj.sTrial  = 2*G*(e - ep);
+            xiTrial     = obj.sTrial - beta;
+            normXiTrial = norm(eig(xiTrial));
             ftrial      = normXiTrial - sqrt(2/3) * K_n;
             
             if ftrial<=0
+                obj.plastic = false;
                 
                 Eh= E/(1-2*v)/(1+v);
                 
@@ -116,11 +139,14 @@ classdef ClassicPlasticityRI
                 ctan = reshape(D([1,4,6,4,2,5,6,5,3],[1,4,6,4,2,5,6,5,3]),3,3,3,3);
                 
             else
+                obj.plastic = true;
                 n = xiTrial ./ normXiTrial;
-                deltaEps = obj.eps_np1(:,:,gp.i, gp.iel) - obj.eps_n(:,:,gp.i, gp.iel);
-%                 [~,Kp,~,Hp] = obj.hardening(alpha_n);
-                deltaGamma = sum(dot(n,deltaEps))/(1+(Kp+Hp)/(3*G));
-                alpha_np1 = alpha_n + sqrt(2/3)*deltaGamma;                
+                obj.dir = n;
+%                 deltaEps = obj.eps_np1(:,:,gp.i, gp.iel) - obj.eps_n(:,:,gp.i, gp.iel);
+                %                 [~,Kp,~,Hp] = obj.hardening(alpha_n);
+                deltaGamma = ftrial / (2*G + (2/3)*(Kp+Hp));
+                obj.dGamma = deltaGamma;
+                alpha_np1 = alpha_n + sqrt(2/3)*deltaGamma;
                 
                 obj.e_p  (:,:,gp.i, gp.iel)= ep + deltaGamma.*n;
                 obj.Beta (:,:,gp.i, gp.iel)= beta + deltaGamma*(2/3)*Hp*n;
@@ -131,7 +157,8 @@ classdef ClassicPlasticityRI
                 %                 sigma= kappa*trace(eps)*I + 2*G*(e - ep);
                 
                 theta   = 1 - (2*G*deltaGamma)/normXiTrial;
-                thetaBar= deltaGamma + theta - 1;
+%                 thetaBar= deltaGamma + theta - 1;
+                thetaBar= 1 / (1 + (Kp+Hp)/(3*G)) + theta - 1;                
                 
                 ctan = zeros(3,3,3,3);
                 for i=1:3
@@ -141,8 +168,7 @@ classdef ClassicPlasticityRI
                                 ctan(i,j,k,l) = ...
                                     kappa*I(i,j)*I(k,l) + ...
                                     2*G*theta* (I4(i,j,k,l) - (1/3)*I(i,j)*I(k,l)) - ...
-                                    2*G*thetaBar*n(i,j)*n(k,l);
-%                                     2*G* (n(i,j)*n(k,l)) / (1 + (Hp+Kp)/(3*G));    
+                                    2*G*thetaBar*n(i,j)*n(k,l); 
                             end
                         end
                     end
@@ -155,7 +181,6 @@ classdef ClassicPlasticityRI
                     ctan(2,3,1,1) ctan(2,3,2,2) ctan(2,3,3,3) ctan(2,3,1,2) ctan(2,3,2,3) ctan(2,3,1,3)
                     ctan(1,3,1,1) ctan(1,3,2,2) ctan(1,3,3,3) ctan(1,3,1,2) ctan(1,3,2,3) ctan(1,3,1,3)];
             end
-            
             
             obj.eps_n(:,:,gp.i, gp.iel)= obj.eps_np1(:,:,gp.i, gp.iel);
         end
@@ -175,27 +200,27 @@ classdef ClassicPlasticityRI
     
     methods (Static)
         
-%         function [deltaGamma, alpha_np1] = box3d1(alpha, normXiTrial, G)
-%             tol = 1e-9;
-%             g = inf;
-%             
-%             deltaGamma = 0;
-%             alpha_k = alpha;
-%             [~,~,H_n,~] = ClassicPlasticityRI.hardening(alpha);
-%             
-%             while(abs(g)>tol)
-%                 [K,Kp,H,Hp] = ClassicPlasticityRI.hardening(alpha_k);
-%                 
-%                 g = -sqrt(2/3)*K + normXiTrial...
-%                     -( 2*G*deltaGamma + sqrt(2/3)*(H - H_n) );
-%                 
-%                 Dg= -2*G*( 1 + (Hp+Kp)/(3*G) );
-%                 
-%                 deltaGamma = deltaGamma - g/Dg;
-%                 alpha_k      = alpha + sqrt(2/3)*deltaGamma;
-%             end
-%             alpha_np1 = alpha_k;
-%         end
+        %         function [deltaGamma, alpha_np1] = box3d1(alpha, normXiTrial, G)
+        %             tol = 1e-9;
+        %             g = inf;
+        %
+        %             deltaGamma = 0;
+        %             alpha_k = alpha;
+        %             [~,~,H_n,~] = ClassicPlasticityRI.hardening(alpha);
+        %
+        %             while(abs(g)>tol)
+        %                 [K,Kp,H,Hp] = ClassicPlasticityRI.hardening(alpha_k);
+        %
+        %                 g = -sqrt(2/3)*K + normXiTrial...
+        %                     -( 2*G*deltaGamma + sqrt(2/3)*(H - H_n) );
+        %
+        %                 Dg= -2*G*( 1 + (Hp+Kp)/(3*G) );
+        %
+        %                 deltaGamma = deltaGamma - g/Dg;
+        %                 alpha_k      = alpha + sqrt(2/3)*deltaGamma;
+        %             end
+        %             alpha_np1 = alpha_k;
+        %         end
         
         function [K,Kp,H,Hp] = hardening(alpha)
             K = (60 + 10*alpha);
