e60cba7e6737eed8a4b3b3a49764abb76443f25d
Omar Nassif <omar.maher.nassif@gmail.com>
2018-09-10 15:06:21 -0400

    Fixed a mistake when applying forces at nodes rather than surfaces.
    Also shortened the script by 30 lines !!

--- generateBC.m
+++ generateBC.m
@@ -1,9 +1,9 @@
 function [new_BC, new_FORCE] = generateBC(old_BC, old_FORCE, nde, el, elmtype, ndm, numnp, numel)
-%% Generate Faces 
+%% Generate Faces
 fc =Faces(elmtype, el, numel);
 
 %% BC
-new_BC = 0;
+new_BC = [];
 for i=1:size(old_BC,1)
    switch  old_BC{i,1}
       case 'x'
@@ -17,16 +17,14 @@ for i=1:size(old_BC,1)
          batch = true;
       case 'node'
          affected_node = old_BC{i,2};
-         starts = new_BC(end,1)+1;
-         ends   = new_BC(end,1)+1;
-         new_BC(starts, 1) = starts;
-         new_BC(starts, 2) = affected_node;
+         starts = size(new_BC,1)+1;
+         ends   = size(new_BC,1)+1;
+         new_BC(starts, 1) = affected_node;
          batch = false;
       otherwise
          error('unrecognized input!!');
    end
-   if batch
-      
+   if batch      
       lmt = old_BC{i,2};
       if sign(lmt) >=0
          affected_nodes = find( nde(:,dir)<=lmt*1.001 & nde(:,dir)>=lmt*0.999 );
@@ -34,41 +32,23 @@ for i=1:size(old_BC,1)
          affected_nodes = find( nde(:,dir)>=lmt*1.001 & nde(:,dir)<=lmt*0.999 );
       end
       
-%       nBC = [affected_nodes , dir*ones(size(affected_nodes)), zeros(size(affected_nodes))];
-%       fc.nodes = affected_nodes;
-%       for ifc = 1:size(fc.faces, 1)
-%          fc.ifc = ifc;
-%          fc.coor = nde(fc.faces(ifc,:), (1:ndm ~= dir));
-%          for igp = 1:fc.numGP
-%             fc.gp.i = igp;
-%             if igp == 1 % No need to compute J for each gp
-%                fc.gp.dXdxi = (fc.gp.dNdxi*fc.coor)';
-%                J = det(fc.gp.dXdxi);
-%             end
-%             nBC(fc.indices, 3) = nBC(fc.indices, 3) + fc.gp.N'*old_BC{i,4}*J*fc.gp.w;
-%          end
-%       end
-      starts = new_BC(end,1)+1;
-      ends   = new_BC(end,1)+length(affected_nodes);
-      new_BC(starts:ends, 1) = starts:ends;
-      new_BC(starts:ends, 2) = affected_nodes;
+      starts = size(new_BC,1)+1;
+      ends   = size(new_BC,1)+length(affected_nodes);
+      new_BC(starts:ends, 1) = affected_nodes;
    end
    
    switch old_BC{i,3}
       case 'u'
-         new_BC(starts:ends, 3) = 1;
+         new_BC(starts:ends, 2:4) = repmat([1, old_BC{i,4}, i], ends-starts+1,1);
       case 'v'
-         new_BC(starts:ends, 3) = 2;
+         new_BC(starts:ends, 2:4) = repmat([2, old_BC{i,4}, i], ends-starts+1,1);
       case 'w'
-         new_BC(starts:ends, 3) = 3;
+         new_BC(starts:ends, 2:4) = repmat([3, old_BC{i,4}, i], ends-starts+1,1);
       otherwise
          error('unrecognized input!!');
-   end
-   new_BC(starts:ends, 4) = old_BC{i,4};
-   new_BC(starts:ends, 5) = i;
-   
+   end  
 end
-new_BC = new_BC(:,2:5);
+
 rowsToDelete = [];
 for i = 1:numnp
    targetNode = find(new_BC(:,1)==i);
@@ -105,14 +85,10 @@ for i=1:size(old_FORCE,1)
          dir = 3;
          batch = true;
       case 'node'
-         if isempty(new_FORCE)
-            new_FORCE = 0;
-         end
          affected_node = old_FORCE{i,2};
-         starts = new_FORCE(end,1)+1;
-         ends   = new_FORCE(end,1)+1;
-         new_FORCE(starts, 1) = starts;
-         new_FORCE(starts, 2) = affected_node;
+         starts = size(new_FORCE,1)+1;
+         ends   = size(new_FORCE,1)+1;
+         new_FORCE(starts, :) = [affected_node,0,0,0];
          batch = false;
       otherwise
          error('unrecognized input!!');
@@ -139,23 +115,16 @@ for i=1:size(old_FORCE,1)
       end
       new_FORCE = [new_FORCE; nF];
    else
-   
-   switch old_FORCE{i,3}
-      case 'u'
-         new_FORCE(starts+1:starts+j, 3) = 1;
-      case 'v'
-         new_FORCE(starts+1:starts+j, 3) = 2;
-      case 'w'
-         new_FORCE(starts+1:starts+j, 3) = 3;
-      otherwise
-         error('unrecognized input!!');
+      switch old_FORCE{i,3}
+         case 'u'
+            new_FORCE(starts:ends, 2:4) = [1, old_FORCE{i,4}, i];
+         case 'v'
+            new_FORCE(starts:ends, 2:4) = [2, old_FORCE{i,4}, i];
+         case 'w'
+            new_FORCE(starts:ends, 2:4) = [3, old_FORCE{i,4}, i];
+         otherwise
+            error('unrecognized input!!');
+      end
    end
-   
-   new_FORCE(starts+1:starts+j, 5) = i;
-   end 
-end
-if ~batch
-   new_FORCE = new_FORCE(:,2:5);
 end
 end
-
