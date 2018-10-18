1e9121400d18d1d648f56cdb157d71945854c06a
Omar Nassif <omar.maher.nassif@gmail.com>
2018-10-18 00:26:27 -0400

    Functions to convert between voigt and full

--- T2T1.m
+++ T2T1.m
@@ -1,4 +1,15 @@
-function [B] = T2T1(A)
-B = [A(1,1);A(2,2);A(3,3);A(1,2);A(2,3);A(3,1)];
+function [B] = T2T1(A,opt)
+if length(A)==2 % 2D
+   if opt==1 % stress-like voigt
+      B = [A(1,1);A(2,2);A(1,2)];
+   elseif opt==2 % strain-like voigt
+      B = [A(1,1);A(2,2);2*A(1,2)];
+   end
+elseif length(A)==3 % 3D
+   if opt==1 
+      B = [A(1,1);A(2,2);A(3,3);A(1,2);A(2,3);A(3,1)];
+   elseif opt == 2 
+      B = [A(1,1);A(2,2);A(3,3);2*A(1,2);2*A(2,3);2*A(3,1)];
+   end
 end
-
+end
