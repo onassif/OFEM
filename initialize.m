2a7e007ae9a6e9a9a281c3eb0c3600a64fc045cc
Omar Nassif <omar.maher.nassif@gmail.com>
2023-02-26 00:26:57 -0500

    Fixed indentation for initialize.m

--- initialize.m
+++ initialize.m
@@ -1,8 +1,8 @@
 if ~exist('dyn', 'var')
-   dyn = 0;
+  dyn = 0;
 else
-   num.dyn_steps = dyn_nsteps;
-   num.dyn_time  = dyn_time;  
+  num.dyn_steps = dyn_nsteps;
+  num.dyn_time  = dyn_time;  
 end
 mixed = 0;
 %   voigt size
@@ -28,19 +28,19 @@ realnumnp = size(nodes,1);
 for i =1: numnp
   occurances =  length(hist.conn(hist.conn==i));
   if occurances > maxSharedNode
-     maxSharedNode = occurances;
+    maxSharedNode = occurances;
   end
 end
 hist.nconn = zeros(numnp, maxSharedNode+1);
 for i=1:numnp
-   count = 0;
-   for j =1:numel
-      if( sum(hist.conn(j,:)==i)>0 )
-         count = count+1;
-         hist.nconn(i,count) = j;
-      end
-   end
-   hist.nconn(i,end) = count;
+  count = 0;
+  for j =1:numel
+    if( sum(hist.conn(j,:)==i)>0 )
+      count = count+1;
+      hist.nconn(i,count) = j;
+    end
+  end
+  hist.nconn(i,end) = count;
 end
 
 num.el    = size(elements,1);
@@ -65,10 +65,10 @@ run('identities.m');
 %%%%%%%%%%%%%%%%%%%%% Create objects:
 % Hardening-related
 if exist('cpType','var')   
-   switch slipType
-      case {'FCC', 'fcc'}
-         slip = FCC();
-   end
+  switch slipType
+    case {'FCC', 'fcc'}
+      slip = FCC();
+  end
 end
 
 % Element-related
@@ -78,61 +78,61 @@ el = Elements(elements, nodes, num, globl.U, hist);
 el.mat = cell(length(material),1);
 finiteDisp = 0;
 for i=1:length(material)
-   if length(material) == 1
-      prps = props;
-   else
-      prps = props{i};
-   end
-   switch material(i)
-      case 1
-         el.mat{i} = Elastic(num, prps, ident.threeD.second, dyn);
-      case 2
-         el.mat{i} = HypoElastic(num, prps, ident.threeD.second);
-      case 3
-         el.mat{i} = HyperElastic(num, prps,  ident.threeD.second);
-      case 4
-         el.mat{i} = ViscoPlastic(num, prps, time, ident.threeD.second);
-      case 5
-         el.mat{i} = PlasticRI(num, prps, ident.threeD.second);
-      case 6
-         el.mat{i} = MixedElasticPlaneStrain(num, prps, ident.threeD.second);
-         mixed = 1;
-      case 7
-         el.mat{i} = CP(num, prps, cpType, hardProps, angles, slip, time, ident.threeD.second);
-      case 8
-         el.mat{i} = DG(num, prps, props, ident.threeD.second); 
-      case 9
-         el.mat{i} = DGHyper(num, prps, props, ident.threeD.second); 
-      case 10
-         el.mat{i} = DGCP(num, prps, props, ident.threeD.second); 
-      case 11
-         el.mat{i} = CP2(num, prps, cpType, hardProps, angles, slip, time, ident.threeD.second);
-   end
-   if el.mat{i}.finiteDisp
-      finiteDisp = 1;
-   end
+  if length(material) == 1
+    prps = props;
+  else
+    prps = props{i};
+  end
+  switch material(i)
+    case 1
+      el.mat{i} = Elastic(num, prps, ident.threeD.second, dyn);
+    case 2
+      el.mat{i} = HypoElastic(num, prps, ident.threeD.second);
+    case 3
+      el.mat{i} = HyperElastic(num, prps,  ident.threeD.second);
+    case 4
+      el.mat{i} = ViscoPlastic(num, prps, time, ident.threeD.second);
+    case 5
+      el.mat{i} = PlasticRI(num, prps, ident.threeD.second);
+    case 6
+      el.mat{i} = MixedElasticPlaneStrain(num, prps, ident.threeD.second);
+      mixed = 1;
+    case 7
+      el.mat{i} = CP(num, prps, cpType, hardProps, angles, slip, time, ident.threeD.second);
+    case 8
+      el.mat{i} = DG(num, prps, props, ident.threeD.second); 
+    case 9
+      el.mat{i} = DGHyper(num, prps, props, ident.threeD.second); 
+    case 10
+      el.mat{i} = DGCP(num, prps, props, ident.threeD.second); 
+    case 11
+      el.mat{i} = CP2(num, prps, cpType, hardProps, angles, slip, time, ident.threeD.second);
+  end
+  if el.mat{i}.finiteDisp
+    finiteDisp = 1;
+  end
 end
 
 % gp-related
 switch eltype
-   case 'Q4'
+  case 'Q4'
     gp = Q4(el.mat{1}.finiteDisp);
-   case 'Q9'
+  case 'Q9'
     gp = Q9(el.mat{1}.finiteDisp);
-   case 'T3'
+  case 'T3'
     gp = T3(el.mat{1}.finiteDisp);
-   case 'T6'
+  case 'T6'
     gp = T6(el.mat{1}.finiteDisp);
-   case 'T4'
+  case 'T4'
     gp = T4(el.mat{1}.finiteDisp);
-   case 'T10'
+  case 'T10'
     gp = T10(el.mat{1}.finiteDisp);
-   case 'Q8'
+  case 'Q8'
     gp = Q8(el.mat{1}.finiteDisp);
 end
 gp = gp.shapeIso();
 [gp.det_dXdxi_list, gp.dNdX_list, gp.dXdxi_list] = shapeRef(...
-   nodes, elements(1:numel,1:nen), gp.dNdxi_list);
+  nodes, elements(1:numel,1:nen), gp.dNdxi_list);
 
 % NR-related
 if exist('time','var') && exist('fctr','var')
